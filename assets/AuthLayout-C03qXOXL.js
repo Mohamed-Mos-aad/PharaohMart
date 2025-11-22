e54b0fe534f6d16ac79cefe3f927445a89ef0be7
Mohamed-Mos-aad <ma3268787@gmail.com>
2025-11-22 19:35:55 +0200

    Updates

---
+++ assets/AuthLayout-C03qXOXL.js
@@ -0,0 +1,1 @@
+import{j as t,O as a}from"./index-CGZKTDgq.js";import{N as s,F as o}from"./NavBar-CrH74_do.js";import"./index-DD7Rslxi.js";const r="_auth_layout_14er9_3",u={auth_layout:r};function i(){return t.jsxs(t.Fragment,{children:[t.jsxs("div",{className:u.auth_layout,children:[t.jsx(s,{}),t.jsx(a,{})]}),t.jsx(o,{})]})}export{i as default};
